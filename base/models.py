af1c26f0fefc529c4a37a155c1d48cb37dedb177
Eldiiar <eldi96kg@gmail.com>
2021-10-12 22:31:51 +0600

    'master'

---
+++ base/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Task(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE , null=True, blank=True,verbose_name='Автор' )
+    title = models.CharField(max_length=200,verbose_name='Название')
+    description = models.TextField(null=True, blank=True,verbose_name='Описание')
+    complete = models.BooleanField(default=False,verbose_name='Завершен')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Создано')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['created']
